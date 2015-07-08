a728a0ae766128d237d48e1a8953d15935d26e69
Falk92 <falkheger@gmail.com>
2015-07-08 18:53:06 +0200

    CF31

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,13 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get '/products/:id', to: 'products#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
