92ae6065f4aa552bc7ed75868f935898f0f6452e
Falk92 <falkheger@gmail.com>
2015-08-27 13:58:10 +0200

    42

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710163035) do
+ActiveRecord::Schema.define(version: 20150827114055) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.integer  "rating"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["product_id"], name: "index_comments_on_product_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "products", force: true do |t|
     t.string   "name"
