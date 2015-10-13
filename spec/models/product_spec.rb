1eff77c888aad0f78da3500ea9d7b2262dbe53be
Falk92 <falkheger@gmail.com>
2015-10-13 14:29:52 +0200

    CF47

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,22 +1,18 @@
 require 'rails_helper'
 
-descripe Product do
+  describe Product do
+    context "average rating" do
 
-  before do
-    @product = Product.create(:name => "Best online shop")
-    @user = User.create(:email => "test@mail.com", :password => "testpassword")
-    @product.comments.create(:rating => 1, :user => @user, :body => "Not really the best shop.")
-    @product.comments.create(:rating => 3, :user => @user, :body => "The middle best shop.")
-    @product.comments.create(:rating => 5, :user => @user, :body => "Really the best.")
-  end
-
-  context "GET #index" do
     before do
-      sign_in @user
-      get :index, id: @user.id
+      @product = Product.create(:name => "Best online shop")
+      @user = User.create(:email => "test@mail.com", :password => "testpassword")
+      @product.comments.create(:rating => 1, :user => @user, :body => "Not really the best shop.")
+      @product.comments.create(:rating => 3, :user => @user, :body => "The middle best shop.")
+      @product.comments.create(:rating => 5, :user => @user, :body => "Really the best.")
     end
-    
-  it "returns the average rating of all comments" do
-    expect(@product.average_rating).to eq "3"
+
+    it "returns the average rating of all comments" do
+      expect(@product.average_rating).to eq 3.0
+    end
   end
 end
