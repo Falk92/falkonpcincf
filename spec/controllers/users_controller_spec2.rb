f255a1c2458e46f2b6bbdde7c5c3597f4bf3036f
Falk92 <falkheger@gmail.com>
2015-10-14 22:04:31 +0200

    CF48

--- spec/controllers/users_controller_spec2.rb
+++ spec/controllers/users_controller_spec2.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 describe UsersController, :type => :controller do
   before do
-    @user1 = User.create(:email => "test@mail.com", :password => "testpassword")
-    @user2 = User.create(:email => "test2@mail.com", :password => "testpassword")
+    @user = User.create(:email => "test@mail.com", :password => "testpassword")
+    @user = User.create(:email => "test2@mail.com", :password => "testpassword")
   end
 
   describe "GET #show" do
-    context "User is logged in" do
+    context "Wrong User is logged in" do
       before do
-        sign_in @user2
-        get :show, id: @user2
-        response.should be_success
+        sign_in @user.2
+        get :show, id: @user.1
+      end
+      it "response with failure" do
         expect(response).to have_http_status(302)
-        expect(response).to redirect_to('lol')
+        expect(response).to redirect_to('root_path')
       end
     end
   end
